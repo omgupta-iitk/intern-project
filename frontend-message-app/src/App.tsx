f211b123d72714f1e4e467bbc3c64bbd6ec32879
Om Gupta <omgupta23@iitk.ac.in>
2025-05-12 00:17:40 +0000

    Some Changes

--- frontend-message-app/src/App.tsx
+++ frontend-message-app/src/App.tsx
@@ -5,7 +5,9 @@ import ChatPage from './components/ChatPage';
 import Navigation from './components/Navigation';
 import './App.css';
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+// TODO: Add your Clerk Publishable Key to the .env file
+// I have added it here for you to test the app
+const PUBLISHABLE_KEY = "pk_test_cmVzdGVkLXdvbGYtNzQuY2xlcmsuYWNjb3VudHMuZGV2JA"
 
 if (!PUBLISHABLE_KEY) {
   throw new Error('Add your Clerk Publishable Key to the .env file')
--- frontend-message-app/src/main.tsx
+++ frontend-message-app/src/main.tsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+// TODO: Add your Clerk Publishable Key to the .env file
+// I have added it here for you to test the app
+const PUBLISHABLE_KEY = "pk_test_cmVzdGVkLXdvbGYtNzQuY2xlcmsuYWNjb3VudHMuZGV2JA"
 
 if (!PUBLISHABLE_KEY) {
   throw new Error('Add your Clerk Publishable Key to the .env file')
